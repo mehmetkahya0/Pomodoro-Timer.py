1e52cdf58a02f8ab592b8092cba92f3e8740570f
Mehmet Kahya <84154488+mehmetkahya0@users.noreply.github.com>
2023-07-24 23:51:54 +0300

    Update main.py
    bug fixes & added some functions

--- main.py
+++ main.py
@@ -1,76 +1,30 @@
-# Mehmet Kahya - 18.06.2022 - Python 3.8.2 - Tkinter - Pomodoro Timer  
-
 import tkinter as tk
-from tkinter import ttk
-from tkinter import messagebox
-from tkinter import Label, Entry, StringVar, IntVar, Tk, ttk
-from tkinter import filedialog
+from tkinter import ttk, messagebox
 import time
 import datetime
 
-
-#Create tkinter window
+# Create tkinter window
 root = tk.Tk()
-root.title("Pomodoro Timer🍅") 
-root.geometry("280x200")
+root.title("Pomodoro Timer🍅")
+root.geometry("290x200")
+root.resizable(False, False)
 
-#Create today total pomodoro count
+# Create today total pomodoro count
 today_total_pomodoro_count = []
 
-#Create a label to display the time
-time_label = tk.Label(root, text="00:00:00", font=("Helvetica", 30))
+# Create a label to display the time
+time_label = tk.Label(root, text="00:00:00", font=("SF pro", 30, "bold"))
 time_label.pack()
 
-
-def setTime():
-    global pomodoro_length
-    pomodoro_length = tk.IntVar()
-    pomodoro_length = int(pomodoro_length_entry.get())*60
-    print(pomodoro_length)
-
-
-timer = None
-stop_length = 60 * 5
+pomodoro_length = 0
+start_time = None
 stop_time = None
- 
-def timer():
-    global start_time
-    global stop_time
-    if start_time is not None:
-        #Calculate the time since start_time
-        time_since_start = time.time() - start_time
-        #Calculate the time left
-        time_left = pomodoro_length - time_since_start
-        #Convert time left to a string
-        time_left_string = str(datetime.timedelta(seconds=time_left))
-        #Display the time left
-        time_label.config(text=time_left_string)
-        #Call the timer again after 1 second
-        root.after(1000, timer)
-    elif stop_time is not None:
-        #Calculate the time since stop_time
-        time_since_stop = time.time() - stop_time
-        #Calculate the time left
-        time_left = stop_length - time_since_stop
-        #Convert time left to a string
-        time_left_string = str(datetime.timedelta(seconds=time_left))
-        #Display the time left
-        time_label.config(text=time_left_string)
-        #Call the timer again after 1 second
-        root.after(1000, timer)
 
-    if time_left <= 0:
-        reset_timer()
-        messagebox.showinfo("Pomodoro Timer", "Pomodoro completed!")
-        print("Pomodoro completed!")
-        today_total_pomodoro_count.append(1)
-        print(len(today_total_pomodoro_count))
-        total = len(today_total_pomodoro_count)
-        #total pomodoro count label
-        total_pomodoro_count_label = ttk.Label(root, text="Total Pomodoro Count: {}".format(total))
-        total_pomodoro_count_label.place(x=10, y=50)
+def set_time():
+    global pomodoro_length
+    pomodoro_length = int(pomodoro_length_entry.get()) * 60
+    print(f"Timer set {pomodoro_length} seconds ({pomodoro_length/60} minutes)")
 
-#Create functions
 def start_timer():
     global start_time
     start_time = time.time()
@@ -81,54 +35,66 @@ def stop_timer():
     start_time = None
 
 def reset_timer():
-    global start_time
+    global start_time, stop_time
     start_time = None
     stop_time = None
     time_label.config(text="00:00:00")
 
+def timer():
+    global start_time, stop_time, pomodoro_length
+    if start_time is not None:
+        time_since_start = time.time() - start_time
+        time_left = max(0, pomodoro_length - time_since_start)
+    elif stop_time is not None:
+        time_since_stop = time.time() - stop_time
+        time_left = max(0, stop_length - time_since_stop)
 
-#Create a frame for the timer
+    # Format the time left as hours:minutes:seconds
+    time_left_string = str(datetime.timedelta(seconds=time_left))
+    # Remove milliseconds from the formatted string
+    time_left_string = time_left_string.split(".")[0]
+    time_label.config(text=time_left_string)
+
+    if time_left <= 0:
+        reset_timer()
+        messagebox.showinfo("Pomodoro Timer", "Pomodoro completed!")
+        print("Pomodoro completed!")
+        today_total_pomodoro_count.append(1)
+        total = len(today_total_pomodoro_count)
+        total_pomodoro_count_label.config(text="Total Pomodoro Count: {}".format(total))
+
+    root.after(1000, timer)
+
+# Create a frame for the timer
 timer_frame = ttk.Frame(root, padding="3 3 12 12")
 timer_frame.pack(side="top", fill="both", expand=True)
 timer_frame.columnconfigure(0, weight=1)
 timer_frame.rowconfigure(0, weight=1)
 
+# Create a label and entry to display the pomodoro length
+pomodoro_length_label = ttk.Label(root, text="Pomodoro Length:", font=("SF pro", 12, "bold"))
+pomodoro_length_label.place(x=10, y=55)
 
-#Create variables
-current_time = tk.StringVar()
-current_time.set("00:00:00")
-current_time_stopwatch = tk.StringVar()
-current_time_stopwatch.set("00:00:00")
-current_time_pomodoro = tk.StringVar()
-current_time_pomodoro.set("00:00:00")
-current_time_pomodoro_stopwatch = tk.StringVar()
-current_time_pomodoro_stopwatch.set("00:00:00")
-
-
-
-
-#Create a label and entry to display the pomodoro length
-pomodoro_length_label = ttk.Label(root, text="Pomodoro Length:")
-pomodoro_length_label.place(x=10, y=50)
-
-pomodoro_length_entry = tk.IntVar()
-pomodoro_length_entry = ttk.Entry(root, width=8)
+pomodoro_length_entry = ttk.Entry(root, width=8, font=("SF pro", 15, "bold"))
 pomodoro_length_entry.place(x=120, y=50)
 
-
-pomodoro_length_button = ttk.Button(root, text="Set", command=setTime)
+pomodoro_length_button = ttk.Button(root, text="Set", command=set_time)
 pomodoro_length_button.place(x=190, y=50)
 
-
 # Create Buttons and Labels
-button_start = ttk.Button(root, text="Start", command=start_timer)
+button_start = ttk.Button(root, text="Start", command=start_timer, width=3.5)
 button_start.pack(side="left")
 
-button_stop = ttk.Button(root, text="Stop", command=stop_timer)
+# button_break = ttk.Button(root, text="Break", width=3.5)
+# button_break.pack(side="left")
+
+button_stop = ttk.Button(root, text="Stop", command=stop_timer, width=3.5)
 button_stop.pack(side="left")
 
-button_reset = ttk.Button(root, text="Reset", command=reset_timer)
+button_reset = ttk.Button(root, text="Reset", command=reset_timer, width=3.5)
 button_reset.pack(side="left")
 
+total_pomodoro_count_label = ttk.Label(root, text="Total Pomodoro Count: 0", font=("SF pro", 15, "bold"))
+total_pomodoro_count_label.place(x=10, y=80)
 
 root.mainloop()
